81b51e51cd69343bd65efb4561fb8d0dd2b9458f
Peter Ihimire <peterihimire@gmail.com>
2021-01-04 00:46:57 +0100

    Worked on the cartItem remove, increase and decrease funtionalities via dispatch , payload , mapDispatchToProps, reducer and more

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,7 +3,15 @@ import { FaCaretUp, FaCaretDown } from "react-icons/fa";
 import { connect } from "react-redux";
 import { INCREASE, DECREASE, REMOVE } from "../actions";
 
-const CartItem = ({ img, title, price, amount, remove }) => {
+const CartItem = ({
+  img,
+  title,
+  price,
+  amount,
+  remove,
+  increase,
+  decrease,
+}) => {
   return (
     <div className="cart-item">
       <img src={img} alt={title} width="50px" height="50px" />
@@ -15,18 +23,28 @@ const CartItem = ({ img, title, price, amount, remove }) => {
         </button>
       </div>
       <div>
-        <button className="amount-btn">
+        <button className="amount-btn" onClick={() => increase()}>
           <FaCaretUp />
         </button>
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+        <button className="amount-btn" onClick={() => decrease()}>
           <FaCaretDown />
         </button>
       </div>
     </div>
   );
 };
-const mapDispatchToProps = (dispatch) => {
-  return { remove: () => dispatch({ type: REMOVE }) };
+const mapDispatchToProps = (dispatch, ownProps) => {
+  console.log(ownProps);
+  const { id, amount } = ownProps;
+  console.log(id, amount);
+  return {
+    remove: () => dispatch({ type: REMOVE, payload: { id } }),
+    increase: () => dispatch({ type: INCREASE, payload: { id, amount } }),
+    decrease: () => dispatch({ type: DECREASE, payload: { id, amount } }),
+  };
 };
 export default connect(null, mapDispatchToProps)(CartItem);
+// The first argument is mapStateToProps, and the second is mapDispatchToProps, since we already got the props, we skipped the first argument by adding 'null' and use the mapDispatchToProps which is the second argument.
+
+// Payload helps us work with our id
--- src/reducer.js
+++ src/reducer.js
@@ -7,13 +7,40 @@ function reducer(state, action) {
     return { ...state, cart: [] };
   }
   if (action.type === DECREASE) {
-    console.log("You decreased");
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter((cartItem) => {
+        return cartItem.id !== action.payload.id;
+      });
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+    return { ...state, cart: tempCart };
   }
   if (action.type === INCREASE) {
-    console.log("You increased");
+    let tempCart = state.cart.map((cartItem) => {
+      console.log(cartItem);
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
   }
   if (action.type === REMOVE) {
-    console.log("You remove");
+    console.log(action.payload.id);
+    console.log(state.cart);
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => {
+        return cartItem.id !== action.payload.id;
+      }),
+    };
   }
   return state;
 }
